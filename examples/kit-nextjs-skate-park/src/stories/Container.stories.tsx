c280f5a782d0d5ba4414774dacf7a75f63213313
Neli Kostadinova <110887014+sc-nelikostadinova@users.noreply.github.com>
2025-08-26 17:30:16 +0300

    Common controls and style adjustments (#17)

--- examples/kit-nextjs-skate-park/src/stories/Container.stories.tsx
+++ examples/kit-nextjs-skate-park/src/stories/Container.stories.tsx
@@ -3,15 +3,59 @@ import { Default as Container } from '../components/container/Container';
 import { ComponentProps } from 'react';
 import { renderStorybookPlaceholder } from './helpers/renderStorybookPlaceholder';
 import { CommonParams, CommonRendering } from './common/commonData';
+import {
+  BackgroundColorArgs,
+  backgroundColorArgTypes,
+  defaultBackgroundColorArgs,
+  defaultHighlightArgs,
+  defaultLayoutArgs,
+  HighlightArgs,
+  HighlightArgTypes,
+  LayoutArgs,
+  LayoutArgTypes,
+} from './common/commonControls';
+import { createRichTextField } from './helpers/createFields';
 
-type StoryProps = ComponentProps<typeof Container>;
+type StoryProps = ComponentProps<typeof Container> &
+  BackgroundColorArgs &
+  HighlightArgs &
+  LayoutArgs & {
+    boxed?: boolean;
+    bordered?: boolean;
+    centered?: boolean;
+  };
 
 const meta = {
   title: 'Components/Container',
   component: Container,
   tags: ['autodocs'],
-  parameters: {
-    layout: 'fullscreen',
+  argTypes: {
+    ...backgroundColorArgTypes,
+    ...HighlightArgTypes,
+    ...LayoutArgTypes,
+    boxed: {
+      name: 'Boxed',
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    bordered: {
+      name: 'Bordered',
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+    centered: {
+      name: 'Centered',
+      control: { type: 'boolean' },
+      defaultValue: false,
+    },
+  },
+  args: {
+    ...defaultBackgroundColorArgs,
+    ...defaultHighlightArgs,
+    ...defaultLayoutArgs,
+    boxed: false,
+    bordered: false,
+    centered: false,
   },
 } satisfies Meta<StoryProps>;
 export default meta;
@@ -29,14 +73,61 @@ const baseRendering = {
 };
 
 export const Default: Story = {
-  render: () => {
+  render: (args) => {
     return (
       <Container
-        params={baseParams}
+        params={{
+          ...baseParams,
+          styles: `${baseParams.styles}
+            ${args.boxed ? 'boxed' : ''}
+            ${args.bordered ? 'sxa-bordered' : ''}
+            ${args.centered ? 'container' : ''}
+            ${args.BackgroundColor}
+            ${args.Highlight.join(' ')}
+            ${args.Indent.join(' ')}
+            ${args.ContentAlignment.join(' ')}
+          `,
+        }}
         rendering={{
           ...baseRendering,
           placeholders: {
             [`container-${baseParams.DynamicPlaceholderId}`]: [renderStorybookPlaceholder()],
+          },
+        }}
+      />
+    );
+  },
+};
+
+export const WithText: Story = {
+  render: (args) => {
+    return (
+      <Container
+        params={{
+          ...baseParams,
+          styles: `${baseParams.styles}
+            ${args.boxed ? 'boxed' : ''}
+            ${args.bordered ? 'sxa-bordered' : ''}
+            ${args.centered ? 'container' : ''}
+            ${args.BackgroundColor}
+            ${args.Highlight.join(' ')}
+            ${args.Indent.join(' ')}
+            ${args.ContentAlignment.join(' ')}
+          `,
+        }}
+        rendering={{
+          ...baseRendering,
+          placeholders: {
+            [`container-${baseParams.DynamicPlaceholderId}`]: [
+              {
+                ...CommonRendering,
+                componentName: 'RichText',
+                params: CommonParams,
+                fields: {
+                  Text: createRichTextField(3),
+                },
+              },
+            ],
           },
         }}
       />
